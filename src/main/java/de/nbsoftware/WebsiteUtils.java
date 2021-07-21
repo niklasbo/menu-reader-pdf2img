d9e697bba29985c6665adb3fe079a5aaacb1d131
Niklas Bölter <bot811@t-online.de>
2021-07-21 18:39:23 +0200

    removed ssl common name checks

--- src/main/java/de/nbsoftware/WebsiteUtils.java
+++ src/main/java/de/nbsoftware/WebsiteUtils.java
@@ -8,30 +8,36 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
-import java.util.Scanner;
+
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLSession;
+
+import org.apache.commons.io.IOUtils;
 
 /**
  * Utils for interacting with websites
  */
 public class WebsiteUtils {
+    static {
+        HttpsURLConnection.setDefaultHostnameVerifier(new HostnameVerifier() {
+            public boolean verify(String hostname, SSLSession session) {
+                return true;
+            }
+        });
+    }
 
     public static String scanWebsiteToText(String urlToRead) throws IOException {
-        String htmlText;
         URL url = new URL(urlToRead);
-        try (Scanner sc = new Scanner(url.openStream(), StandardCharsets.UTF_8.toString())) {
-            sc.useDelimiter("\\A");
-            StringBuffer sb = new StringBuffer();
-            while (sc.hasNext()) {
-                String s = sc.next();
-                sb.append(s);
-            }
-            htmlText = sb.toString();
-        }
-        return htmlText;
+
+        InputStream in = url.openStream();
+        String body = IOUtils.toString(in, StandardCharsets.UTF_8);
+
+        return body;
     }
 
     public static void downloadPdf(String urlToPdf, File pdf) throws IOException {
-        URL url = new URL(urlToPdf);
+        URL url = new URL(urlToPdf.replaceAll(" ", "%20"));
         try (InputStream in = url.openStream()) {
             Files.copy(in, Paths.get(pdf.getAbsolutePath()), StandardCopyOption.REPLACE_EXISTING);
         }
